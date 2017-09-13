2ec4cb6039f267e113a2718cb0c023af60c23002
gmanhr <zvonimir.kristo010@gmail.com>
2017-09-13 11:59:43 +0200

    Crawl dodana malo klasa

--- Crawl.py
+++ Crawl.py
@@ -1,3 +1,7 @@
+import Page
+
 class Crawl:
-    def __init__(url):
-        self.
+    def __init__(url_arg, max_iter_arg, max_time_arg):
+        self.max_iter = max_iter_arg
+        self.max_time = max_time_arg
+        self.start_url = url_arg
