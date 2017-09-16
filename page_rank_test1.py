6db561b1bd12a4237ed7b9568bccdd84e2dd609a
gmanhr <zvonimir.kristo010@gmail.com>
2017-09-16 10:43:43 +0200

    popravljeno

--- page_rank_test1.py
+++ page_rank_test1.py
@@ -2,15 +2,16 @@ from class_test1 import Page
 import networkx as nx
 import matplotlib.pyplot as plt
 
+MAX_ITER = 30  # broj iteracija page_rank algoritma
 
-MAX_ITER = 30  #broj iteracija page_rank algoritma
+# zamisilio sam pageRank kao void funkciju koja nista ne vraca vec samo mijenja pagerank vrijednosti svakog cvora,
+# elementa liste
+# isto tako bilo bi dobro (mozda) da imamo posebno listu sa svim stranicama i posebno graf s tim elementima liste
+# (uvjeren sam da je laksa iteracija i da je sve poslozenije i lakse je trazit stvari) -> tako sam i koncipirao sam
+# pagerank
+# i tako sam i igradio ovu sample listu
 
-#zamisilio sam pageRank kao void funkciju koja nista ne vraca vec samo mijenja pagerank vrijednosti svakog cvora, elementa liste
-#isto tako bilo bi dobro (mozda) da imamo posebno listu sa svim stranicama i posebno graf s tim elementima liste
-# (uvjeren sam da je laksa iteracija i da je sve poslozenije i lakse je trazit stvari) -> tako sam i koncipirao sam pagerank
-#i tako sam i igradio ovu sample listu
-
-### kako pagerank funkcionira
+# ## kako pagerank funkcionira
 # d - dampling factor
 # (obično se stavlja 0.85)
 #
@@ -25,27 +26,26 @@ MAX_ITER = 30  #broj iteracija page_rank algoritma
 # možemo početi s vrijednošču 1
 
 
-#funkciji se prosljedjuje i lista i graf
-def pageRank(graph_arg,list_arg):
+# funkciji se prosljedjuje i lista i graf
+def pageRank(graph_arg, list_arg):
 
     d = 0.85
     global MAX_ITER
 
     for k in range(MAX_ITER):
-
         for i in range(len(list_arg)):
             temp_pr = 0
             for j in range(len(list_arg)):
-                if j != i and graph_arg.has_edge(l[j],l[i]):
+                if j != i and graph_arg.has_edge(l[j], l[i]):
                     temp_pr += list_arg[j].page_rank/graph_arg.out_degree(list_arg[j])
             temp_pr = (1-d) + d * temp_pr
 
-            list_arg[i].page_rank=temp_pr
+            list_arg[i].page_rank = temp_pr
 
     return
 
 
-#kreiranje sample liste iz koje se kreira i sam graf
+# kreiranje sample liste iz koje se kreira i sam graf
 l = []
 
 l.append(Page(1, "index"))
@@ -59,38 +59,39 @@ l.append(Page(7, "slike"))
 test_graf = nx.DiGraph()
 test_graf.add_nodes_from(l)
 
-test_graf.add_edge(l[0],l[1])
-test_graf.add_edge(l[0],l[2])
-test_graf.add_edge(l[0],l[5])
-test_graf.add_edge(l[0],l[6])
+test_graf.add_edge(l[0], l[1])
+test_graf.add_edge(l[0], l[2])
+test_graf.add_edge(l[0], l[5])
+test_graf.add_edge(l[0], l[6])
 
-test_graf.add_edge(l[1],l[0])
-test_graf.add_edge(l[2],l[0])
-test_graf.add_edge(l[5],l[0])
-test_graf.add_edge(l[6],l[0])
+test_graf.add_edge(l[1], l[0])
+test_graf.add_edge(l[2], l[0])
+test_graf.add_edge(l[5], l[0])
+test_graf.add_edge(l[6], l[0])
 
-test_graf.add_edge(l[2],l[3])
-test_graf.add_edge(l[2],l[4])
+test_graf.add_edge(l[2], l[3])
+test_graf.add_edge(l[2], l[4])
 #####
 
-#poziv page_rank funkcije
-pageRank(test_graf,l)
+# poziv page_rank funkcije
+pageRank(test_graf, l)
 
-#prl lista je dosta sklepan nacin da matplotlib nacrta cvorove cija velicina prati page rank (jer treba proslijediti array)
-#posto cemo u listu spremati kad i u graf nebi trebalo biti problema s indexima (da se pomijesaju recimo)
-#na test primjeru radi i crta dobru sliku
+# prl lista je dosta sklepan nacin da matplotlib nacrta cvorove cija velicina prati page rank (jer treba proslijediti
+# array)
+# posto cemo u listu spremati kad i u graf nebi trebalo biti problema s indexima (da se pomijesaju recimo)
+# na test primjeru radi i crta dobru sliku
 
 prl = []
-#ispisuje page_rank vrijednosti i radi prl listu
+# ispisuje page_rank vrijednosti i radi prl listu
 for i in range(len(l)):
     print(l[i].page_rank)
     prl.append(l[i].page_rank*300)
 
-#printa info grafa -> broj cvorova edgova itd... za testiranje
+# printa info grafa -> broj cvorova edgova itd... za testiranje
 print(nx.info(test_graf))
 
-#crta graf
-nx.draw_networkx(test_graf,node_size =prl)
+# crta graf
+nx.draw_networkx(test_graf, node_size=prl)
 plt.show()
 
-#Abramovic
+# Abramovic
