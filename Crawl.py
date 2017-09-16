78c6b876629eb806ee77004b72c3f682253afdd0
AbramovicHrvoje <hrvoje.abramovic@yahoo.com>
2017-09-16 11:54:49 +0200

    slika

--- Crawl.py
+++ Crawl.py
@@ -17,6 +17,8 @@ class Crawl:
         self.page_array = []
         self.q = queue
         self.graph = nx.DiGraph()
+        self.links_iter =  []
+        self.page_ranks =  []
 
         # self.home_page = Page.Page(0, url_arg)
 
@@ -29,7 +31,7 @@ class Crawl:
         visited_pages = set()
         found_pages = set()
         found_mails = set()
-        links_iter = []
+
 
         for i in range(20):
             self.print(len(page_queue))
@@ -45,16 +47,46 @@ class Crawl:
                 for local_url in local_links:
                     self.graph.add_edge(current_page, local_url)
                     if local_url not in visited_pages and local_url not in found_pages:
-                        links_iter.append(local_url)
+                        self.links_iter.append(local_url)
+                        self.page_ranks.append(1.0)
                         page_queue.append(local_url)
                         found_pages |= {local_url}
                         self.print(local_url)
 
         self.print(visited_pages)
+
+        self.pageRank(self.graph,self.links_iter)
+        #prl = []
+       # for i in range(len(self.links_iter)):
+            #print(l[i].page_rank)
+        #    prl.append(self.links_iter[i].page_rank * 300)
+        for i in range(len(self.page_ranks)):
+            self.page_ranks[i] *= 210
+            #self.print(self.page_ranks[i])
+        nx.draw_spring(self.graph, node_size=self.page_ranks, node_shape = 's')
+        plt.savefig("slikica.png",node_size=self.page_ranks)
         return visited_pages, found_mails
 
+    def pageRank(self,graph_arg, list_arg):
+
+        d = 0.77
+        MAX_ITER = 15
+
+        for k in range(MAX_ITER):
+            for i in range(len(list_arg)):
+                temp_pr = 0
+                for j in range(len(list_arg)):
+                    if j != i and graph_arg.has_edge(self.links_iter[j], self.links_iter[i]):
+                        temp_pr += self.page_ranks[j] / graph_arg.out_degree(list_arg[j])
+                temp_pr = (1 - d) + d * temp_pr
+
+                self.page_ranks[i] = temp_pr
+
+        return
+
+
 if __name__ == '__main__':
-    c = Crawl('icm.hr', 10**6, 10**6)
+    c = Crawl('icm.hr', 10**6, 10**6,2000)
     r = c.crawl()
-    nx.draw_random(c.graph)
+    #nx.draw_random(c.graph)
     plt.show()
